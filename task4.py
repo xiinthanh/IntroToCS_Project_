ca9d9567d211467d8d9f0bfbca3a6dacc0551142
xiinthanh <118944173+xiinthanh@users.noreply.github.com>
2023-10-24 16:00:29 +0700

    Add initial files

---
+++ task4.py
@@ -0,0 +1,7 @@
+class Task4:
+    def __init__(self):
+        print("Init task 4")
+        return
+
+    def Task4_Run(self):
+        print("Task 4 is activated!!!!")
---
+++ task5.py
@@ -0,0 +1,7 @@
+class Task5:
+    def __init__(self):
+        print("Init task 5")
+        return
+
+    def Task5_Run(self):
+        print("Task 5 is activated!!!!")
