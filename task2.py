753c46313ed06b0c5b02f7069f3782d42e7d8c19
xiinthanh <118944173+xiinthanh@users.noreply.github.com>
2023-11-26 16:35:52 +0700

    update for writing report

--- task2.py
+++ task2.py
@@ -1,5 +1,4 @@
 import cv2
-import matplotlib.pyplot as plt
 from ultralytics import YOLO
 from camera import *
 import time
@@ -7,23 +6,19 @@ import time
 class FaceRecognizer:
     file_path = '.\\AI\\FaceRecognition.pt'
 
-    def __init__(self, _ID, _task_Ready, _task_Data, _func):
+    def __init__(self, _ID, _task_Ready, _task_Data, _camera):
         print("Init task 2")
         
         # Load the custom model
         self.model = YOLO(self.file_path)
+        self.camera = _camera
 
         self.ID = _ID
         self.task_Ready = _task_Ready
         self.task_Data = _task_Data
-        self.grabImage = _func
 
     def Run(self):
         '''return a string student id if recognizable, "Unkown" otherwise'''
-
-        if (not self.task_Ready[self.ID]):  # There is no data
-            return "[i]"
-
         print("#"*30)
         print("Face detecting...")
         
@@ -32,11 +27,11 @@ class FaceRecognizer:
         for i in range(10):  # Try detect for 10 times
             time.sleep(0.2)
             print(f"Attempt {i+1}th")
-            # Get and save to ".\images\from_cam.png"
-            self.grabImage()
+            # Get and save to "self.camera.save_path" (".\images\from_cam.png")
+            self.camera.Run()
 
             # Open image
-            image = cv2.imread(".\\images\\from_cam.png")
+            image = cv2.imread(self.camera.save_path)
             
             # Perform classification on the frame
             results = self.model(image)
@@ -54,12 +49,20 @@ class FaceRecognizer:
             if (confidence > 70):
                 result = predicted_id
                 break
+        
+        return result
+    
+
+    def RunTask(self):
+        if (not self.task_Ready[self.ID]):  # There is no data
+            return "[i]"
+
+        result = self.Run()
 
         # Creating the cycle: task1 -> task2 -> task3 -> task4
         if (result != "Unknown"):
             self.task_Ready[self.ID + 1] = True
             self.task_Data[self.ID + 1] = result
-
         else:
             # Restart the cycle: return to the first task
             self.task_Ready[0] = True
@@ -67,8 +70,16 @@ class FaceRecognizer:
         self.task_Ready[self.ID] = False
 
         return ("[1]" + result)
-
+    
 # Create an instance of the Task2 class
 if __name__ == "__main__":
-    recognizer = FaceRecognizer()
-    print(recognizer.Run())
+
+    task_Ready = [False, True, False, False]
+    task_Data = ["", "", "", ""]
+
+    camera = Camera()
+
+    task2 = FaceRecognizer(1, task_Ready, task_Data, camera)
+    while True:
+        print(task2.Run())
+        time.sleep(3)
