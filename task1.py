2f79ab01adbae61c352c16d59e9b55712deb28fd
xiinthanh <118944173+xiinthanh@users.noreply.github.com>
2023-11-26 16:35:22 +0700

    update for writing report

--- task1.py
+++ task1.py
@@ -8,33 +8,30 @@ class StudentCardDetector:
     model_path = ".\\AI\\keras_Model.h5"
     labels_path = ".\\AI\\labels.txt"
 
-    def __init__(self, _ID, _task_Ready, _task_Data, _func):
+    def __init__(self, _ID, _task_Ready, _task_Data, _camera):
         print("Init task 1")
         # Load model & labels
         self.model = load_model(self.model_path, compile=False)
         self.class_names = open(self.labels_path, "r").readlines()
 
+        # camera object
+        self.camera = _camera
+
         self.ID = _ID
         self.task_Ready = _task_Ready
         self.task_Data = _task_Data
-        # tell the camera to capture the image
-        self.grabImage = _func
 
     def Run(self):
-        # There is no data to run
-        if (not self.task_Ready[self.ID]):
-            return "[i]"
-
         print("#"*30)
         print("Student Card detecting...")
-    
-        # Get and save to ".\images\from_cam.png"
-        self.grabImage()
+
+        # Get and save to "self.camera.save_path" (".\images\from_cam.png")
+        self.camera.Run()
 
         is_student_card = "False"
 
         # Open image
-        image = cv2.imread(".\\images\\from_cam.png")
+        image = cv2.imread(self.camera.save_path)
         # Modify the image for detection
         image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
         image = (image / 127.5) - 1
@@ -47,9 +44,20 @@ class StudentCardDetector:
         print("Class:", class_name[2:], end="")
         print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
 
+        # Qualified-Condition
         # If dectect student card with over 70% confidence, confirm
         if (index == 0 and confidence_score > 0.70):
             is_student_card = "True"
+
+        return is_student_card
+    
+
+    def RunTask(self):
+        # There is no data to run
+        if (not self.task_Ready[self.ID]):
+            return "[i]"
+
+        is_student_card = self.Run()
         
         # Creating the cycle: task1 -> task2 -> task3 -> task4
         # First task in the chain
@@ -61,13 +69,16 @@ class StudentCardDetector:
 
         return ("[0]" + is_student_card)
 
+
 # Test
 if __name__ == "__main__":
+    import time
+
     camera = Camera()
     task_Ready = [True, True, True, True]
     task_Data = ["", "", "", ""]
-    detector = StudentCardDetector(0, task_Ready, task_Data, camera.Run)
+    detector = StudentCardDetector(0, task_Ready, task_Data, camera)
     while True:
         print(detector.Run())
-        # time.sleep(1)
+        time.sleep(3)
 
